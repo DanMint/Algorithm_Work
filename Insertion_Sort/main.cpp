11740787af3f0f482cc1b1f4f726f3e0126a25ef
Daniel Mints <67702241+DanMint@users.noreply.github.com>
2022-11-17 22:30:52 -0500

    Create main.cpp

---
+++ Insertion_Sort/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+void insertion_sort (std::vector<int> &arr) {
+    int i = 1;
+    while (i < arr.size()) {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            int temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j -= 1;
+        }
+        i += 1;
+    }
+}
+
+int main() {
+
+    std::vector<int> arr = {3, 2, 1, 6, 7, 10, 4};
+    insertion_sort(arr);
+    for (const auto &el : arr)
+        std::cout << el << " ";
+
+    return 0;
+}
